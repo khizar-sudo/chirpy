8a60ad9eaf35dca4016eb7239c3beb81a8515cfe
Khizar <khizarnawab1@outlook.com>
2026-01-14 02:36:25 +0500

    feat: add metrics

--- main.go
+++ main.go
@@ -1,21 +1,49 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
+	cfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", healthCheck)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /healthz", healthCheck)
+	mux.HandleFunc("GET /metrics", cfg.getMetrics)
+	mux.HandleFunc("POST /reset", cfg.resetMetrics)
 
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
 
 func healthCheck(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
